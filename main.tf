f6eb7c14a4a021f9302da1c6bd25daf5e1282fa3
Ahmad Rashidh <ahmadrashidh@mac.mul.ie.ibm.com>
2025-07-10 14:32:25 +0100

    pr for action demo

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.3"
+    }
+  }
+}
+provider "local" {
+  # Configuration options
+}
+data "local_file" "readme" {
+  filename = "${path.module}/README.md"
+}
